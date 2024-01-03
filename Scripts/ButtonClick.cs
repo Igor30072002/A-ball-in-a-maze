cbaac9eba9bde05e2add2dd157ddb6a9a83fe1b0
Igor <drugakovigor751@gmail.com>
2024-01-03 12:37:06 +0300

    Create ButtonClick.cs

---
+++ Scripts/ButtonClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+
+    public void Click()
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+}
