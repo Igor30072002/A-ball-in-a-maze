4336aaa17fef2383bc9312efcf1c5607d345c606
Igor <drugakovigor751@gmail.com>
2024-01-03 12:37:57 +0300

    Create CrysralNumber.cs

---
+++ Scripts/CrysralNumber.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrysralNumber : MonoBehaviour
+{
+    public Text crystalNumberText;
+    private int number;
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        number = 0;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            audioSource.clip = audioClip;
+            audioSource.Play();
+            other.gameObject.SetActive(false);
+            number++;
+        }
+    }
+
+    void Update()
+    {
+        crystalNumberText.text = number.ToString();
+    }
+}
