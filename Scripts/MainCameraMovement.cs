44d9c18af420a244f447e4187323a88dbf6ed69e
Igor <drugakovigor751@gmail.com>
2024-01-03 12:38:30 +0300

    Create MainCameraMovement.cs

---
+++ Scripts/MainCameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraMovement : MonoBehaviour
+{
+    private Vector3 startinPoint;
+    private float timer;
+    void Start()
+    {
+        timer = 0;
+        startinPoint = transform.position;
+    }
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+        timer += Time.deltaTime;
+        if(timer > 30)
+        {
+            transform.position = startinPoint;
+            timer = 0;
+        }
+    }
+}
