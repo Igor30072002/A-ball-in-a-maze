0c0e3e04ed17f69471418d2a9ecd2fe46634ded0
Igor <drugakovigor751@gmail.com>
2024-01-03 12:38:44 +0300

    Create moving.cs

---
+++ Scripts/moving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        Move();
+    }
+
+    private Vector2 IsThereTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            return Input.GetTouch(0).deltaPosition;
+        }
+        else return Vector2.zero;
+    }
+
+    private void Move()
+    {
+        Physics.gravity = new Vector3(IsThereTouch().x * speed, Physics.gravity.y, IsThereTouch().y * speed);
+    }
+}
