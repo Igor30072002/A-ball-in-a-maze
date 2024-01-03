1d8928b0a37b3d90adaf5cae12eeeb03701ee1a3
Igor <drugakovigor751@gmail.com>
2024-01-03 12:38:21 +0300

    Create Loss.cs

---
+++ Scripts/Loss.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loss : MonoBehaviour
+{
+    public timer timer;
+
+    public GameObject lossPanel;
+
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+    
+    private bool oneTime = false;
+    void Update()
+    {
+        if(timer.intTime <= 0)
+        {
+            if (!oneTime)
+            {
+                audioSource.clip = audioClip;
+                audioSource.Play();
+                oneTime = true;
+            }
+            lossPanel.SetActive(true);
+        }
+    }
+}
