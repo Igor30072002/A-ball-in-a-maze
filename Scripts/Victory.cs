b48567edcc219f3a5182b367be39bdc663cfe827
Igor <drugakovigor751@gmail.com>
2024-01-03 12:39:05 +0300

    Create Victory.cs

---
+++ Scripts/Victory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    private GameObject ball;
+    [SerializeField] private GameObject victoryPanel;
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+    void Start()
+    {
+        ball = GetComponent<GameObject>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Finish")
+        {
+            audioSource.clip = audioClip;
+            audioSource.Play();
+            Time.timeScale = 0;
+            victoryPanel.SetActive(true);
+        }
+    }
+}
