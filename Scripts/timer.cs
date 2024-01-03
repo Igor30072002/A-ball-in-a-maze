112f533d3e5578071ae8275ccb089afc58a3e627
Igor <drugakovigor751@gmail.com>
2024-01-03 12:38:56 +0300

    Create timer.cs

---
+++ Scripts/timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    private Text textTimer;
+    [SerializeField] private float time;
+    public GameObject ball;
+    public int intTime;
+    void Start()
+    {
+        textTimer = GetComponent<Text>();
+        intTime = (int)time;
+        textTimer.text = intTime.ToString();
+    }
+    void Update()
+    {
+        time -= Time.deltaTime;
+        intTime = (int)time;
+        if (intTime <= 10)
+        {
+            textTimer.color = Color.red;
+            textTimer.fontSize = 70;
+        }
+        if (intTime <= 0)
+        {
+            ball.SetActive(false);
+            textTimer.gameObject.SetActive(false);
+        }
+        textTimer.text = intTime.ToString();
+    }
+}
