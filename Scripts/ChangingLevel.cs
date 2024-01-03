5a89febf69fdd561c0c5c71d1d32dab8a283c76d
Igor <drugakovigor751@gmail.com>
2024-01-03 12:37:44 +0300

    Create ChangingLevel.cs

---
+++ Scripts/ChangingLevel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangingLevel : MonoBehaviour
+{
+    private int currentLevel;
+
+    void Start()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(currentLevel);
+    }
+
+    public void NextLevel()
+    {
+        Time.timeScale = 1;
+        if (currentLevel == 4) SceneManager.LoadScene(1);
+        else SceneManager.LoadScene(currentLevel + 1);
+    }
+
+    public void ToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
